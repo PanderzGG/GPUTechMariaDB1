c76e0ae26a3817d57b4f6c7d73f0501a445304fd
PanderzGG <saschakleinen91@gmail.com>
2024-06-07 18:56:30 +0200

    Part of the GPU Update

--- PCHardwareDbSkripte/05-05_Misc_Search_Code.sql
+++ PCHardwareDbSkripte/05-05_Misc_Search_Code.sql
@@ -44,3 +44,59 @@ select GPUChip, count(*)
 select Chipsatz, MaxMemory, COUNT(*)
     from mobo
         group by Chipsatz;
+
+
+# Überprüfen
+
+
+
+/*
+SELECT MAX(x) FROM y;
+
+SELECT MIN(x) FROM y;
+
+SELECT * FROM x
+WHERE y = 310.95;
+
+# mit variable
+SELECT MAX(y)
+INTO @maxY
+FROM x;
+
+SELECT round (@maxPreis, 2);
+
+# SET @maxPreis = 310.95;
+
+SELECT * FROM x WHERE y = @maxY;
+
+
+# sub query
+
+SELECT * FROM x
+WHERE y =
+      (
+
+          SELECT MAX( y ) FROM x
+
+      );
+
+#Durchschnittspreis holen und runden
+SELECT round( avg( x ), 2 ) AS 'Something' FROM x;
+
+SELECT z AS 'z',
+       y AS 'y'
+FROM x;
+
+SELECT COUNT(*) AS AnzahlDerDatensaetze FROM x;
+SELECT COUNT(*) AS AnzahlDerDatensaetze FROM x;
+
+SELECT SUM( x * 3 ) FROM y;
+
+SELECT * FROM x
+ORDER BY y DESC
+LIMIT 5; # LIMIT schaut die ersten 5 ergebnisse die gefunden werden
+
+SELECT * FROM x
+ORDER BY y DESC
+LIMIT 5 OFFSET 10; # Offset verschiebt die Suche auf die nächsten ergebnisse also von Ergebnis 6 bis 10
+*/
