78f88d50bd897c19103318f2b12159c9471de8af
PanderzGG <125514785+PanderzGG@users.noreply.github.com>
2024-06-11 11:27:26 +0200

    Update 02-01_Daten_Hersteller.sql

--- PCHardwareDbSkripte/02-01_Daten_Hersteller.sql
+++ PCHardwareDbSkripte/02-01_Daten_Hersteller.sql
@@ -3,5 +3,10 @@ use pchardware;
 insert into hersteller values
     (1, 'AMD'),
     (2, 'Nvidia'),
-    (3, 'Intel')
-  ;
+    (3, 'Intel'),
+    (4,'Asus'),
+    (5, 'MSI'),
+    (6, 'Gigabyte'),
+    (7, 'ASRock'),
+    (8, 'NZXT')
+    ;
