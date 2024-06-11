b21b396039a64051f09972b8c89f36ef97fa1f68
PanderzGG <125514785+PanderzGG@users.noreply.github.com>
2024-06-11 11:28:07 +0200

    Update 01-04_Table_Mobo.sql

--- PCHardwareDbSkripte/01-04_Table_Mobo.sql
+++ PCHardwareDbSkripte/01-04_Table_Mobo.sql
@@ -3,7 +3,11 @@ use pchardware;
 create table Mobo(
 
                      Mobo_ID INT PRIMARY KEY auto_increment,
-                     Hersteller VARCHAR (40),
+                     Hersteller INT,
+
+                     FOREIGN KEY M_Hersteller_FK(Hersteller)
+                         REFERENCES Hersteller(Hersteller_ID),
+
                      Produktname VARCHAR (60),
                      Sockel VARCHAR (8),
                      Chipsatz VARCHAR (8),
