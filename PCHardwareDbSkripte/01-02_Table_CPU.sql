f35f75b7a4fafe4f36ed0f11ffc9687786ee2a4c
PanderzGG <125514785+PanderzGG@users.noreply.github.com>
2024-06-11 11:10:01 +0200

    Update 01-03_Table_CPU.sql

--- PCHardwareDbSkripte/01-02_Table_CPU.sql
+++ PCHardwareDbSkripte/01-02_Table_CPU.sql
@@ -1,17 +1,21 @@
-use pchardware;
+USE pchardware;
 
-create table CPU(
+CREATE TABLE CPU(
 
-    CPU_ID INT PRIMARY KEY auto_increment,
-    Hersteller VARCHAR (30),
-    ProduktName VARCHAR (40),
-    CodeName VARCHAR (40),
-    Released DATE,
-    Kerne VARCHAR (40),
-    Takt VARCHAR (20),
-    Sockel VARCHAR (15),
-    Process VARCHAR (8),
-    L3Cahce VARCHAR (8),
-    TDP VARCHAR (10)
+                    CPU_ID INT PRIMARY KEY AUTO_INCREMENT,
+                    Hersteller_ID INT,
+
+                    FOREIGN KEY C_Hersteller_FK(Hersteller_ID)
+                        REFERENCES hersteller(Hersteller_ID),
+
+                    ProduktName VARCHAR (40),
+                    CodeName VARCHAR (40),
+                    Released DATE,
+                    Kerne VARCHAR (40),
+                    Takt VARCHAR (20),
+                    Sockel VARCHAR (15),
+                    HerstellGröße VARCHAR (8),
+                    L3Cahce VARCHAR (8),
+                    TDP VARCHAR (10)
 
 );
