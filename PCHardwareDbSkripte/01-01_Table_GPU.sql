b33dd14c7f324bc85136e7100b36b26eec369170
PanderzGG <125514785+PanderzGG@users.noreply.github.com>
2024-06-11 11:04:21 +0200

    Update 01-01_Table_GPU.sql

--- PCHardwareDbSkripte/01-01_Table_GPU.sql
+++ PCHardwareDbSkripte/01-01_Table_GPU.sql
@@ -3,8 +3,12 @@ USE pchardware;
 CREATE TABLE GPU(
 
                     GPU_ID INT PRIMARY KEY AUTO_INCREMENT,
-                    #Hersteller VARCHAR (30),
+                    
                     Hersteller_ID INT,
+
+                    FOREIGN KEY G_Hersteller_FK(Hersteller_ID)
+                        REFERENCES hersteller(Hersteller_ID),
+                    
                     ProduktName VARCHAR (50),
                     GPUChip VARCHAR (30),
                     Released DATE,
@@ -13,5 +17,4 @@ CREATE TABLE GPU(
                     GPUClock VARCHAR (30),
                     MemoryClock VARCHAR (30),
                     ShaderTMURop VARCHAR (30)
-
 );
